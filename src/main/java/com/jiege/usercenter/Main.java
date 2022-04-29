13337bb0c464e4fc1f6edd094d953b07f83e4ea5
wuyujie <wuyujie@qq.com>
2022-04-30 02:27:13 +0800

    Initial commit

---
+++ src/main/java/com/jiege/usercenter/Main.java
@@ -0,0 +1,8 @@
+package com.jiege.usercenter;
+
+
+
+public class Main {
+
+
+}
---
+++ src/main/java/com/jiege/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.jiege.usercenter.mapper;
+
+import com.jiege.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity com.yupi.usercenter.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
